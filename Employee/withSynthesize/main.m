e26a034bfe2ed1ba19feff18899d9b5feba52969
Prasoon Telang <prasoontelang@gmail.com>
2013-09-01 22:49:00 +0530

    Employee program

--- Employee/withSynthesize/main.m
+++ Employee/withSynthesize/main.m
@@ -4,9 +4,9 @@ int main (int argc, const char * argv[])
 {
 NSAutoreleasePool *pool = [[NSAutoreleasePool alloc]init];
 Emp *myEmp = [[Emp alloc]init];
-NSNumber *empId=[NSNumber numberWithInt:125];
+NSNumber *empId=[NSNumber numberWithInt:113];
 [myEmp setEmpId:empId];
-[myEmp setName:@"Aayush" andDept:@"ISE"];
+[myEmp setName:@"Prasoon Telang" andDept:@"ISE"];
 NSLog(@"Employee Details %@ %@ %@",[myEmp name],[myEmp dept],[myEmp empId]);
 [myEmp release];
 [pool drain];
